cd81375a2da07e2e761e5f5f673a6abff3829919
doubleZ <doubleZ0108@163.com>
2020-11-12 21:54:29 +0800

    pop up full screen vipcard

--- miniprogram/components/wall/vipcard/vipcard.js
+++ miniprogram/components/wall/vipcard/vipcard.js
@@ -11,13 +11,16 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    scaleFactor: 0
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    taggleVipCardTap: function() {
+      let that = this;
+      this.setData({ scaleFactor: 1 - that.data.scaleFactor })
+    }
   }
 })
