602d7f32e5f85a2623424e9a0893c6c160a26ce8
doubleZ <doubleZ0108@163.com>
2020-11-16 17:25:30 +0800

    :tada:basic comment page is on-line

---
+++ miniprogram/pages/comment/comment.js
@@ -0,0 +1,87 @@
+// miniprogram/pages/comment/comment.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    commentItem: [
+      {
+        avatarSrc: "../../resource/img/avatar/avatar.jpg",
+        name: "名字",
+        content: "这是一条很长的评论,这是一条很长的评论,这是一条很长的评论,这是一条很长的评论,这是一条很长的评论,这是一条很长的评论,这是一条很长的评论这是一条很长的评论,这是一条很长的评论,这是一条很长的评论,这是一条很长的评论"
+      },
+      {
+        avatarSrc: "../../resource/img/avatar/avatar.jpg",
+        name: "名字",
+        content: "这是一条很短的评论"
+      },
+      {
+        avatarSrc: "../../resource/img/avatar/avatar.jpg",
+        name: "名字",
+        content: "这是一条很长的评论,这是一条很长的评论,这是一条很长的评论,这是一条很长的评论,这是一条很长的评论,这是一条很长的评论,这是一条很长的评论这是一条很长的评论,这是一条很长的评论,这是一条很长的评论,这是一条很长的评论"
+      },
+      {
+        avatarSrc: "../../resource/img/avatar/avatar.jpg",
+        name: "名字",
+        content: "这是一条很短的评论"
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
