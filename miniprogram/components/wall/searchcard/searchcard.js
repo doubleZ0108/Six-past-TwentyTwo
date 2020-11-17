3dfdc4ef747617d003d4bfc90e0606f22eadd3d1
doubleZ <doubleZ0108@163.com>
2020-11-17 16:13:48 +0800

    :building_construction:add other card

---
+++ miniprogram/components/wall/searchcard/searchcard.js
@@ -0,0 +1,26 @@
+// components/wall/searchcard/searchcard.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    posLeft: {
+      type: String,
+      value: "310%"
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
