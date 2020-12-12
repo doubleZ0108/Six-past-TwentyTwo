74d95e6a3ac3514fefcd2136ece6774b4694ccb7
doubleZ <doubleZ0108@163.com>
2020-12-12 23:54:56 +0800

    about our is link to back-end

--- miniprogram/pages/about/about.js
+++ miniprogram/pages/about/about.js
@@ -1,9 +1,13 @@
 // miniprogram/pages/about/about.js
+
+const db = wx.cloud.database()
+const _ = db.command
+
 Page({
-
   data: {
-
+    about_our_img: null
   },
+
 
   onImageTap: function(e){
     wx.vibrateShort()
@@ -12,4 +16,21 @@ Page({
       urls: [e.target.dataset.imgsrc]
     })
   },
+
+  onLoad: function() {
+    let that = this
+
+    db.collection('contentful')
+    .where({
+      what_is_this: _.eq("AboutOur")
+    })
+    .get({
+      success: function(res) {
+        let aboutData = res.data[0]
+        that.setData({
+          about_our_img: aboutData.imgSrc
+        })
+      }
+    })
+  }
 })
